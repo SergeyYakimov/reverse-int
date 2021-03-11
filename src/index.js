cfa2c2c26d6dc4570e6239ba96964423603a72e3
SergeyYakimov <teriertis@yandex.ru>
2021-03-11 14:42:20 +0800

    feat: make task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (isNaN(n) || !Number.isInteger(n)) {
+        throw new Error();
+    }
+
+    let num = n;
+
+    if (n < 0) {
+        num = Math.abs(n);
+    }
+
+    return +[...String(num)].reverse().join("");
+};
